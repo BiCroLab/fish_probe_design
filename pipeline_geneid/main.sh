17b7dec8999fc349c66dacdb6b4b3059578621c7
salvzzz <92976410+salvzzz@users.noreply.github.com>
2024-11-13 10:33:14 +0100

    Update main.sh
    nHUSH / cQuery can now run one after the other

--- pipeline_geneid/main.sh
+++ pipeline_geneid/main.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#SBATCH --job-name=prb_main --partition=cpuq --time=99:00:00
+#SBATCH --nodes=1 --ntasks-per-node=5 --mem=2G
+
 
 ### Importing Pipeline Modules
 #PIPELINE_MODS="/group/bienko/projects/RNAFISH/Scripts_GeneID/modules"
@@ -9,46 +12,44 @@ PIPELINE_MODS="/scratch/raquel.andre/PROBES/prb_parallel/pipeline_geneid/modules
 
 echo -e "Importing Pipeline Modules:" && for MOD in ${PIPELINE_MODS}/*.sh; do source ${MOD}; done
 
-#define gene list of interest
-#RANDOM_LIST="/group/bienko/projects/RNAFISH/Scripts/pipeline/modules/random.list.txt" 
+### Input Gene List with ENSG gene identifiers
 GENE_LIST="/scratch/raquel.andre/PROBES/Pipeline_LS_20241029/ENSGgenelistFunkGen.txt" 
 
 #define directories
 GENOME="/group/bienko/annotations/human/hg38_GRCh38_p13/genome/Homo_sapiens.GRCh38.103.fa.gz"
 ANNOT_INPUT="/group/bienko/annotations/human/hg38_GRCh38_p13/annotations/gencode/v43/gencode.v43.primary_assembly.basic.annotation.gtf.gz" 
-#WORKDIR="/group/bienko/projects/RNAFISH/Analysis_RNAmode6/Genes"
 #WORKDIR="/scratch/raquel.andre/PROBES/Pipeline_LS_20241029/"
 WORKDIR="/group/bienko/projects/RNAFISH/Output_FunkGlist"
 
+OLIGO_LENGTH=40 ### added default here
+
+
 ### ----------------------------------------------------------------------------------
 ### ----------------------------------------------------------------------------------
 ### ----------------------------------------------------------------------------------
-### METHOD #1: using parallel to speed up initial steps 
-# fun() {
-#     ID=${1}; GENOME=${2}; ANNOT_INPUT=${3}; WORKDIR=${4}
+### METHOD #1: using parallel to speed up initial steps <------ 
+fun() {
+    ID=${1}; GENOME=${2}; ANNOT_INPUT=${3}; WORKDIR=${4}
+    prbDesign001 -i ${ID} -g ${GENOME} -a ${ANNOT_INPUT} -w ${WORKDIR} --ccds-only
+    prbDesign002 -i ${ID} -g ${GENOME} -l ${OLIGO_LENGTH} -w ${WORKDIR} -f "chr1"
+      }
 
-
-#     prbDesign001 -i ${ID} -g ${GENOME} -a ${ANNOT_INPUT} -w ${WORKDIR}
-#     prbDesign002 -i ${ID} -g ${GENOME} -l 40 -w ${WORKDIR} -f "chr1"
-#       }
-
-# export -f fun
-# CPU=8
-# time parallel -j ${CPU} fun ::: $(cat ${RANDOM_LIST}) ::: ${GENOME} ::: ${ANNOT_INPUT} ::: ${WORKDIR}
+INPUT_PARSING_CPU=5 ; export -f fun
+time parallel -j ${INPUT_PARSING_CPU} fun ::: $(cat ${RANDOM_LIST}) ::: ${GENOME} ::: ${ANNOT_INPUT} ::: ${WORKDIR}
 
 
  ### METHOD #2: using while-loops, slower for large numbers
- time while IFS= read -r ID; do
-     prbDesign001 -i ${ID} -g ${GENOME} -a ${ANNOT_INPUT} -w ${WORKDIR} --ccds-only
-     prbDesign002 -i ${ID} -g ${GENOME} -l 40 -w ${WORKDIR} -f "chr1"
- done < ${GENE_LIST}
+ # time while IFS= read -r ID; do
+ #     prbDesign001 -i ${ID} -g ${GENOME} -a ${ANNOT_INPUT} -w ${WORKDIR} --ccds-only
+ #     prbDesign002 -i ${ID} -g ${GENOME} -l ${OLIGO_LENGTH} -w ${WORKDIR} -f "chr1"
+ # done < ${GENE_LIST}
  ### ----------------------------------------------------------------------------------
  ### ----------------------------------------------------------------------------------
  ### ----------------------------------------------------------------------------------
 
  #needs to be run only once per input genome to create a blacklisted region >> to be optimised since if loop is not working as expected
 
-     prbInitLinks -w ${WORKDIR} -g ${GENOME} -l 40 #--link-only
+prbInitLinks -w ${WORKDIR} -g ${GENOME} -l ${OLIGO_LENGTH} #--link-only
 
 
 #output genes from gene_list with empty split/ENSG*/data/rois/all_regions_tsv 
@@ -56,15 +57,18 @@ echo " Looking for genes with have empty rois tsv ..."
 wc -l split/ENSG*/data/rois/all_regions_tsv | awk '$1 == 1 {print $2}' | cut -d'/' -f 2 
 
 #  ##step nHUSH (mers for both strands are calculated)
-#  CPU_PER_JOB=10
+CPU_PER_JOB=10
+MEM_PER_JOB="40G"
+TIME_PER_JOB="25:00:00"
 
-#      slurmArrayLauncher                      \
+#      slurmArrayLauncher \
 #       --command-name "prbRun_nHUSH"    \
-#       --cpu-per-job "$CPU_PER_JOB"                \
-#       --mem-per-job "40G"               \
-#       --time-req "15:00:00"             \
-#       --work-dir ${WORKDIR}              \
-#       --command-args "--work-dir ${WORKDIR} --cpu-per-job ${CPU_PER_JOB}"    
+#       --command-args "--work-dir ${WORKDIR} --cpu-per-job ${CPU_PER_JOB} --length-oligos ${OLIGO_LENGTH}" \
+#       --cpu-per-job "${CPU_PER_JOB}" \
+#       --mem-per-job "${MEM_PER_JOB}" \
+#       --time-req "${TIME_PER_JOB}" \
+#       --work-dir ${WORKDIR}
+
 
 ## output genes that run out of time during nHUSH step
 # for file in ${WORKDIR}/logs/*/*nHUSH*.txt
@@ -80,16 +84,31 @@ wc -l split/ENSG*/data/rois/all_regions_tsv | awk '$1 == 1 {print $2}' | cut -d'
 #     wc -l "$directory/data/rois/all_regions_tsv"
 # done <  ${WORKDIR}/directories.txt
 
-##WAIT until all previous jobs have run >> add dependency ?
 
-##step cQuery
-# CPU_PER_JOB=10
+### <------------------------------ Stall main.sh as long as prbRun_nHUSH jobs are running
 
-#     slurmArrayLauncher                      \
-#      --command-name "prbRun_cQuery"    \
-#      --cpu-per-job "$CPU_PER_JOB"                 \
-#      --mem-per-job "30G"               \
-#      --time-req "15:00:00"             \
-#      --work-dir ${WORKDIR}              \
-#      --command-args "--work-dir ${WORKDIR} --cpu-per-job ${CPU_PER_JOB}" 
+while true; do
+   ### Waiting for all <prbRun_nHUSH> jobs to have finished before starting <prbRun_cQuery>
+   PREVIOUS_JOBS=$(squeue -A ${USER} --array -h --name "prbRun_nHUSH" -o "%.20u %.30j" | wc -l)
+   if [[ ${PREVIOUS_JOBS} == 0 ]]; then echo -e "prbRun_nHUSH ✓   $(date)"; break ; else sleep 60; fi
+done
+
+### <------------------------------ Resume main.sh when prbRun_nHUSH jobs are finished
+
+
+
+### prbRun_cQuery
+
+CPU_PER_JOB=10
+MEM_PER_JOB="30G"
+TIME_PER_JOB="15:00:00"
+
+#     slurmArrayLauncher \
+#      --command-name "prbRun_cQuery" \
+#      --command-args "--work-dir ${WORKDIR} --cpu-per-job ${CPU_PER_JOB} --length-oligos ${OLIGO_LENGTH}" \
+#      --cpu-per-job "${CPU_PER_JOB}" \
+#      --mem-per-job "${MEM_PER_JOB}" \
+#      --time-req "${TIME_PER_JOB}" \
+#      --work-dir ${WORKDIR}
+
 
