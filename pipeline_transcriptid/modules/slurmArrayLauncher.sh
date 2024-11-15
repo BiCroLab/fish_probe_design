19aee87bc54f0202d7117a2a1f2594c10e44b502
salvzzz <92976410+salvzzz@users.noreply.github.com>
2024-11-15 19:03:09 +0100

    Update slurmArrayLauncher.sh

--- pipeline_transcriptid/modules/slurmArrayLauncher.sh
+++ pipeline_transcriptid/modules/slurmArrayLauncher.sh
@@ -4,9 +4,9 @@
 slurmArrayLauncher() {
 
     ### -- Setting default values. Overwritten by user-defined variables.
-    MAX_RUNNING_JOBS=30 ; CPU_PER_JOB=5 ; MEM_PER_JOB="40G" ; RUN_TIME="24:00:00"
+    MAX_RUNNING_JOBS=50 ; CPU_PER_JOB=5 ; MEM_PER_JOB="40G" ; RUN_TIME="24:00:00"
     
-    ARRAY_MAX=200  ### Each SLURM Array will have a maximum of ${ARRAY_MAX} elements
+    ARRAY_MAX=400  ### Each SLURM Array will have a maximum of ${ARRAY_MAX} elements
     HPC_MAX=800    ### No more than ${HPC_MAX} jobs can co-exist in the HPC (both running / pending).
 
 
