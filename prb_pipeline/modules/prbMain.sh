e617f76d4a01cd819362663e46d4f5fe6dc993e4
salvzzz <92976410+salvzzz@users.noreply.github.com>
2024-12-27 12:13:53 +0100

    updated main script

---
+++ prb_pipeline/modules/prbMain.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+
+### -----------------------------------------------------------------------------------
+### -----------------------------------------------------------------------------------
+### -----------------------------------------------------------------------------------
+
+
+### 1. Importing Pipeline Modules
+echo -e "Importing Pipeline Modules:"
+for MOD in $(find "${PIPELINE_MODS}" -name "*.sh" ! -name "prbMain.sh"); do source "${MOD}"; done
+### ----------------------------------------------------------------------------------
+  
+### 2. Input Pre-Processing ----------------------------------------------------------------
+### ----------------------------------------------------------------------------------
+### --- [prbReadInputGTF] Extracting input objects from provided GTF file
+### --- [prbReadInputFasta] Extracting input objects from provided FASTA file
+### --- [prbReadInputBed] Extracting input objects from provided BED file
+
+
+if [[ ${INPUT_GTF} != "" ]]; then 
+
+ prbReadInputGTF         \
+  -i ${INPUT_GTF}        \
+  -g ${GENOME}           \
+  -w ${WORKDIR}          \
+  -l ${OLIGO_LENGTH}     \
+  -s ${SPACING_FACTOR}   \
+  --ccds-only 
+
+fi 
+
+
+if [[ ${INPUT_FASTA} != "" ]]; then
+
+ prbReadInputFasta       \
+  -i ${INPUT_FASTA}      \
+  -g ${GENOME}           \
+  -w ${WORKDIR}          \
+  -l ${OLIGO_LENGTH}     \
+  -s ${SPACING_FACTOR}
+
+fi
+
+
+if [[ ${INPUT_BED} != "" ]]; then
+
+ prbReadInputBed         \
+  -i ${INPUT_BED}        \
+  -g ${GENOME}           \
+  -w ${WORKDIR}          \
+  -l ${OLIGO_LENGTH}     \
+  -s ${SPACING_FACTOR}
+
+fi
+
+
+
+### ----------------------------------------------------------------------------------
+### 3. Adding References -------------------------------------------------------------
+### ----------------------------------------------------------------------------------
+
+### Creating genome and blacklist reference and temporary objects.
+### Finally, link all reference objects to each sub-directory
+
+prbReferenceCreate -w ${WORKDIR} -g ${GENOME} -l ${OLIGO_LENGTH} -s ${OLIGO_SUBLENGTH}
+
+
+
+### ----------------------------------------------------------------------------------
+### 4. Launching Pipeline ------------------------------------------------------------
+### ----------------------------------------------------------------------------------
+
+
+
+
+### Launching prbRun_nHUSH
+CPU_PER_JOB=10 ; MEM_PER_JOB="40G" ; TIME_PER_JOB="05:00:00"
+
+slurmArrayLauncher \
+ --command-name "prbRun_nHUSH"    \
+ --command-args "-c ${CPU_PER_JOB} -l ${OLIGO_LENGTH} -s ${OLIGO_SUBLENGTH}" \
+ --cpu-per-job "${CPU_PER_JOB}" \
+ --mem-per-job "${MEM_PER_JOB}" \
+ --time-req "${TIME_PER_JOB}" \
+ --work-dir ${WORKDIR} \
+ --parallel-jobs 20 \
+ --slurm-array-max 900 \
+ --slurm-hpc-max 950
+
+### Waiting for prbRun_nHUSH jobs to finish
+slurmBlocker --job-name "prbRun_nHUSH" -s 60
+
+
+
+### Launching prbRun_cQuery
+CPU_PER_JOB=10 ; MEM_PER_JOB="40G" ; TIME_PER_JOB="06:00:00"
+
+slurmArrayLauncher \
+ --command-name "prbRun_cQuery" \
+ --command-args "-c ${CPU_PER_JOB} -l ${OLIGO_LENGTH} -s ${OLIGO_SUBLENGTH}" \
+ --cpu-per-job "${CPU_PER_JOB}" \
+ --mem-per-job "${MEM_PER_JOB}" \
+ --time-req "${TIME_PER_JOB}" \
+ --work-dir ${WORKDIR} \
+ --parallel-jobs 20 \
+ --slurm-array-max 935 \
+ --slurm-hpc-max 950
+
+
+
+
+
+
+
+
+
+
+### In case nHUSH crashed and cQuery started, you might want to run:
+
+        # rm -rf ${WORKDIR}/split/regions/*/data/db
+        # rm -rf ${WORKDIR}split/regions/*/data/db_tsv
+        # rm -rf ${WORKDIR}split/regions/*/data/HUSH_c*
+        # rm -rf ${WORKDIR}split/regions/*/data/melt
+        # rm -rf ${WORKDIR}split/regions/*/data/secs
+        # rm -rf ${WORKDIR}split/regions/*/data/probe_candidates
+        # rm -rf ${WORKDIR}split/regions/*/data/logfiles
+        # rm -rf ${WORKDIR}split/regions/*/data/selected_probes
+        # rm -rf ${WORKDIR}split/regions/*/data/final_probes
